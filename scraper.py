db6ca4aa1e636832aeb9cf7a2bf00984f3d8bd2e
rodegayatree6 <rodegayatree6@gmail.com>
2026-01-17 15:19:28 +0530

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,1 +1,23 @@
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+
+url = "https://books.toscrape.com/"
+soup = BeautifulSoup(requests.get(url).text,"html.parser")
+
+data = []
+
+for book in soup.select("article.product_pod"):
+    title = book.h3.a["title"]
+    price = book.select_one(".price_color").text
+    rating = book.select_one(".star-rating")["class"][1]
+
+    data.append([title,price,rating])
+
+df = pd.DataFrame(data,columns=["Title","Price","Rating"])
+
+df.to_csv("books.csv",index=False)
+
+
+print(df)
 
